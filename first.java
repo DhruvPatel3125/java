835fd9d670373668e3fe202e3923d9b933434797
dhruv <dhruvjpatel5@gmail.com>
2024-11-02 01:41:40 +0530

    first commit

---
+++ first.java
@@ -0,0 +1,8 @@
+// java ni under class nu name and file nu name same hovu joiye
+
+public class first {
+    public static void main(String args[]){
+        System.out.print("1,2,3,4,5");
+    }
+    
+}
