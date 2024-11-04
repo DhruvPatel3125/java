9dc50b249b65ef5ee9c916d1184eb45247f9bf23
dhruv <dhruvjpatel5@gmail.com>
2024-11-04 10:50:54 +0530

    datatype in java

---
+++ Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    public static void main(String args[]){
+        byte b = 8;
+        System.out.println(b);
+        String ch = "Dhruv patel";
+        System.out.println(ch);
+        short n =240;
+        System.out.println(n);
+    }
+}
