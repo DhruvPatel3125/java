ec92ff057797e70f9683b89de498c7cd163f766d
dhruv <dhruvjpatel5@gmail.com>
2024-11-04 11:46:37 +0530

    input and sum in java

---
+++ SumOfAandB.java
@@ -0,0 +1,12 @@
+public class SumOfAandB {
+    public static void main(String[] args) {
+        int a = 10; // Declare and initialize variable a
+
+        int b = 20; // Declare and initialize variable b
+
+        int sum = a + b; // Add a and b and store the result in sum
+
+        System.out.println("Sum of a and b is: " + sum);
+
+    }
+}
