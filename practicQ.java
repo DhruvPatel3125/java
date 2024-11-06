721e6dfcc3deab5e3d4da42cf53d89701ad0f1ae
dhruv <dhruvjpatel5@gmail.com>
2024-11-06 20:39:37 +0530

    operator

--- practicQ.java
+++ practicQ.java
@@ -1,3 +1,6 @@
+// In a program, input 3 numbers : A, B and C. You have to output the average othese 3 numbers
+
+
 // import java.util.*;
 
 // public class practicQ {
@@ -14,7 +17,7 @@
 
 // }
 
-
+// In a program, input the side of a square. You have to output the area of the square
 import java.util.*;
 
 public class practicQ {
@@ -28,3 +31,4 @@ public class practicQ {
     }
     
 }
+
